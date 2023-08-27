e93a260da56896d0cd73a4afd412cb7532684902
Ubuntu <ubuntu@ip-172-31-46-5.ap-south-1.compute.internal>
2023-08-27 06:38:28 +0000

    script file

---
+++ install.sh
@@ -0,0 +1,28 @@
+sudo apt update
+sudo apt install git -y
+sudo apt install docker.io -y
+sudo usermod -aG docker ubuntu
+sudo apt-get update && sudo apt-get install -y gnupg software-properties-common
+wget -O- https://apt.releases.hashicorp.com/gpg | \
+gpg --dearmor | \
+sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+gpg --no-default-keyring \
+--keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
+--fingerprint
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
+https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
+sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update
+sudo apt-get install terraform -y
+sudo apt-get update
+# Download the Docker Compose binary
+sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+# Make it executable
+sudo chmod +x /usr/local/bin/docker-compose
+# Verify the installation
+sudo apt update
+docker-compose --version
+docker --version
+git --version
+terraform --version
+
